40c3342e498317e28db25e266adf9e072dd94d71
rigobertoalag <a20150338@utem.edu.mx>
2022-02-14 18:56:48 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,94 @@
+var express = require("express");
+var app = express();
+
+app.listen(3001, () => {
+    console.log("Servidor corriendo en el puerto 3001")
+})
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+app.get("/cities", (req, res, next) => {
+    res.json([
+        {
+            id: 0,
+            destination: "Guadalajara",
+            flights: [
+                {
+                    id: 1,
+                    takeoff: '08/02/2022 11:00:00',
+                    landing: '08/02/2022 12:00:00',
+                    price: 600
+                }
+                ,
+                {
+                    id: 2,
+                    takeoff: '08/02/2022 13:30:00',
+                    landing: '08/02/2022 14:30:00',
+                    price: 200
+                },
+                {
+                    id: 3,
+                    takeoff: '08/02/2022 16:40:00',
+                    landing: '08/02/2022 17:40:00',
+                    price: 300
+                }
+            ]
+        },
+        {
+            id: 1,
+            destination: "Monterrey",
+            flights: [
+                {
+                    id: 1,
+                    takeoff: '09/02/2022 11:00:00',
+                    landing: '09/02/2022 12:00:00',
+                    price: 800
+                }
+                ,
+                {
+                    id: 2,
+                    takeoff: '09/02/2022 13:30:00',
+                    landing: '09/02/2022 14:30:00',
+                    price: 400
+                },
+                {
+                    id: 3,
+                    takeoff: '09/02/2022 16:40:00',
+                    landing: '09/02/2022 18:20:00',
+                    price: 100
+                }
+            ]
+        },
+        {
+            id: 2,
+            destination: "Ciudad de México",
+            flights: [
+                {
+                    id: 1,
+                    takeoff: '10/02/2022 17:00:00',
+                    landing: '10/02/2022 19:00:00',
+                    price: 800
+                }
+                ,
+                {
+                    id: 2,
+                    takeoff: '10/02/2022 14:30:00',
+                    landing: '10/02/2022 16:30:00',
+                    price: 400
+                },
+                {
+                    id: 3,
+                    takeoff: '10/02/2022 18:40:00',
+                    landing: '10/02/2022 20:10:00',
+                    price: 100
+                }
+            ]
+        }
+    ])
+})
